dd7a086b6de349af967737e2b72d91dd640b0693
Bruno Reges <bruno.reges@gmail.com>
2025-04-25 02:37:53 -0300

    Add executeGamCommand method to KubernetesAdapter and export KubernetesWebSocketAdapter in index.js

--- gamgui-server/services/container/index.js
+++ gamgui-server/services/container/index.js
@@ -6,10 +6,12 @@ const ContainerService = require('./ContainerService');
 const KubernetesAdapter = require('./KubernetesAdapter');
 const DockerAdapter = require('./DockerAdapter');
 const ContainerFactory = require('./ContainerFactory');
+const KubernetesWebSocketAdapter = require('./KubernetesWebSocketAdapter');
 
 module.exports = {
   ContainerService,
   KubernetesAdapter,
   DockerAdapter,
-  ContainerFactory
+  ContainerFactory,
+  KubernetesWebSocketAdapter
 };
